0695b576e3cf6be495c222d80664850e1d0cf647
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-02-19 21:56:54 +0330

    Set blur event for inputs and add coment
    اضافه کردن روبداد blur برای ورودی ها و اضافه کردن کامنت.

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,43 @@
-const body = document.querySelector("body"),
+// -- Varibales
+const // Selecting body
+  body = document.querySelector("body"),
+  // Selecting email input
   emailInp = document.querySelector("#email"),
+  // Selecting subject input
   subjectInp = document.querySelector("#subject"),
+  // Selecting message input
   messageInp = document.querySelector("#message"),
+  // Selecting send button
   sendBtn = document.querySelector("#sendBtn");
 
+// -- Events
+// Add event to dom for disable send button
 document.addEventListener("DOMContentLoaded", addImt);
-emailInp.addEventListener('blur', changeBorderColor);
-subjectInp.addEventListener('blur', changeBorderColor);
-messageInp.addEventListener('blur', changeBorderColor);
+// Add event for emailInput blur
+emailInp.addEventListener("blur", validateInput);
+// Add event for subjectInput blur
+subjectInp.addEventListener("blur", validateInput);
+// Add event for messageInput blur
+messageInp.addEventListener("blur", validateInput);
 
-function addImt () {
-    sendBtn.disabled = true;
+// -- Functions
+// Function for disable send button
+function addImt() {
+  // disabling send button
+  sendBtn.disabled = true;
 }
 
-function changeBorderColor () {
-
+// Function for when input blur
+function validateInput() {
+  if (this.value.length > 0) {
+    if (this.type == "email") {
+      if (this.value.includes("@")) {
+        this.style.borderBottomColor = "#43cea2";
+      } else {
+        this.style.borderBottomColor = "red";
+      }
+    } else {
+      this.style.borderBottomColor = "#43cea2";
+    }
+  } else this.style.borderColor = "red";
 }
